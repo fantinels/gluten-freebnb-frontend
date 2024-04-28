3330ba279f0c915482ad20e5f32e598dfd31fcb0
Gui-Menezes <guibenmen@gmail.com>
2024-04-27 21:44:22 -0300

    lang para pt-br e ajuste na responsvidade da lista de acomodações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,62 @@ export class AppComponent {
       text2: 'Vista para o Guaíba',
       text3: '20 - 30 de jun.',
       text4: 'R$ 250,00 noite'
-    }
+    },
+    { 
+      imageUrl: '../../assets/img/logo2.jpg',
+      text1: 'Florianópolis, SC',
+      text2: 'Vista para a paria',
+      text3: '20 - 30 de jul.',
+      text4: 'R$ 295,00 noite'
+    },
+    {
+      imageUrl: '../../assets/img/logo1.PNG',
+      text1: 'São Paulo, SP',
+      text2: 'Próximo ao metrô',
+      text3: '15 - 25 de ago.',
+      text4: 'R$ 175,00 noite'
+    },
+    { 
+      imageUrl: '../../assets/img/logo2.jpg',
+      text1: 'Salvador, BA',
+      text2: 'De frente para o Farol da Bara',
+      text3: '10 - 20 de jan.',
+      text4: 'R$ 450,00 noite'
+    },
+    {
+      imageUrl: '../../assets/img/logo1.PNG',
+      text1: 'Porto Alegre, RS',
+      text2: 'De frente para o parque Germânia',
+      text3: '20 - 30 de jun.',
+      text4: 'R$ 290,00 noite'
+    },
+    { 
+      imageUrl: '../../assets/img/logo2.jpg',
+      text1: 'Porto Alegre, RS',
+      text2: 'Vista para o Guaíba',
+      text3: '20 - 30 de jun.',
+      text4: 'R$ 250,00 noite'
+    },
+    {
+      imageUrl: '../../assets/img/logo1.PNG',
+      text1: 'Porto Alegre, RS',
+      text2: 'Vista para o Guaíba',
+      text3: '20 - 30 de jun.',
+      text4: 'R$ 250,00 noite'
+    },
+    { 
+      imageUrl: '../../assets/img/logo2.jpg',
+      text1: 'Porto Alegre, RS',
+      text2: 'Vista para o Guaíba',
+      text3: '20 - 30 de jun.',
+      text4: 'R$ 250,00 noite'
+    },
+    {
+      imageUrl: '../../assets/img/logo1.PNG',
+      text1: 'Porto Alegre, RS',
+      text2: 'Vista para o Guaíba',
+      text3: '20 - 30 de jun.',
+      text4: 'R$ 250,00 noite'
+    },
   ];
 }
