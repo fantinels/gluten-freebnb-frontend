c2183e55b7505e9d7dfd7803b3b1a8957ecb0dc9
Gui-Menezes <guibenmen@gmail.com>
2024-04-29 22:19:59 -0300

    inclusão do botão ver mais e alteração no layout dos cards

--- src/app/acomodacao.ts
+++ src/app/acomodacao.ts
@@ -1,8 +1,23 @@
 export class Acomodacao {
     id?: number;
+    id_usuario?: number;
     nome?: string;
     especificacao?: string;
     periodo?: string;
-    preco?: string;
-    imagem?: string;
+    cep?: string;
+    tipo_logradouro?: string;
+    logradouro?: string;
+    numero?: number;
+    complemento?: string;
+    bairro?: string;
+    cidade?: string;
+    uf?: string;
+    descricao?: string;
+    foto?: string;
+    tipo_acomodacao?: string;
+    valor?: string;
+    qt_hospede?: number;
+    qt_banheiro?: number;
+    qt_quarto?: number;
+    qt_cama?: number;
 }
