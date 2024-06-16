03b5ddbce680ef7abcfb3779d130c9c97a4be771
Paola Fantinel <fantinelpaola15@gmail.com>
2024-06-16 12:02:53 -0300

    Ajuste filtro de pesquisa e adição do campo especificação

--- src/app/filtro-pesquisa.pipe.ts
+++ src/app/filtro-pesquisa.pipe.ts
@@ -7,10 +7,23 @@ import { Acomodacao } from './acomodacao';
 })
 export class FiltroPesquisaPipe implements PipeTransform {
 
-  transform(litsaAcomodacoes: Acomodacao[], nomePesq: string): Acomodacao[] {
-    return litsaAcomodacoes.filter ( (acomodacao:Acomodacao) => {
-      return acomodacao.cidade?.toLowerCase().includes(nomePesq.toLowerCase())
-    } )
+  // transform(litsaAcomodacoes: Acomodacao[], nomePesq: string): Acomodacao[] {
+  //   return litsaAcomodacoes.filter ( (acomodacao:Acomodacao) => {
+  //     return acomodacao.cidade?.toLowerCase().includes(nomePesq.toLowerCase())
+  //   } )
+  // }
+
+  transform(listaAcomodacoes: Acomodacao[], termoPesquisa: string): Acomodacao[] {
+    if (!termoPesquisa) {
+      return listaAcomodacoes;
+    }
+
+    termoPesquisa = termoPesquisa.toLowerCase();
+
+    return listaAcomodacoes.filter((acomodacao: Acomodacao) => {
+      return acomodacao.nome?.toLowerCase().includes(termoPesquisa) || 
+             acomodacao.cidade?.toLowerCase().includes(termoPesquisa);
+    });
   }
 
 }
