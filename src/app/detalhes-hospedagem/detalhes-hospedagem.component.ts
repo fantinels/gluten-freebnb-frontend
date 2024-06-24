5821a937d66656982ec1c955608521af857da147
Gui-Menezes <guibenmen@gmail.com>
2024-06-24 17:33:58 -0300

    estrutura tela detalhes hospedagem

--- src/app/detalhes-hospedagem/detalhes-hospedagem.component.ts
+++ src/app/detalhes-hospedagem/detalhes-hospedagem.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AcomodacaoService } from '../acomodacao.service';
 
 @Component({
   selector: 'app-detalhes-hospedagem',
   templateUrl: './detalhes-hospedagem.component.html',
-  styleUrl: './detalhes-hospedagem.component.css'
+  styleUrl: './detalhes-hospedagem.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DetalhesHospedagemComponent {
+export class DetalhesHospedagemComponent implements OnInit {
   id: any;
   hospedagem: any = '';
 
@@ -17,12 +18,22 @@ export class DetalhesHospedagemComponent {
       this.id = params['id'];
     })
 
+    // this.acomodacaoService.buscarHospedagemPorIdHospedagem(this.id).subscribe((hosp) => {
+    //   this.hospedagem = hosp;
+    //   // console.log("hosp", hosp);
+    //   console.log("hospedagem", this.hospedagem);
+    // })
+    
+  }
+
+  ngOnInit(): void {
     this.acomodacaoService.buscarHospedagemPorIdHospedagem(this.id).subscribe((hosp) => {
       this.hospedagem = hosp;
-      console.log("hosp", hosp);
+      // console.log("hosp", hosp);
       console.log("hospedagem", this.hospedagem);
+      console.log("foto", this.hospedagem.foto);
+      console.log("nome", this.hospedagem.nome);
     })
-    
   }
 
 
